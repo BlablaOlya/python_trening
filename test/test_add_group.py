c36900aa392d3e73b0e6858b28bfb7d4cdb9cad7
Popova Olga Olegovna <olgapo@ozon.ru>
2021-01-16 23:00:01 +0300

    Group. Выделен класс для работы с группами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,11 +12,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="test", header="123", footer="123"))
+    app.gpoup.create(Group(name="test", header="123", footer="123"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.gpoup.create(Group(name="", header="", footer=""))
     app.session.logout()
