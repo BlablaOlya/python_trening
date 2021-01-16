01ab7d61cce33c3b42d28eb362655803459acd41
Popova Olga Olegovna <olgapo@ozon.ru>
2021-01-16 23:13:24 +0300

    Contact. Выделен класс для работы с контактами, удален application_contact.py, изменен application_group.py  на application.py

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,6 +1,6 @@
 import pytest
 from model.contact import Contact
-from fixture.application_contact import Application
+from fixture.application import Application
 
 
 @pytest.fixture
@@ -11,12 +11,12 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.create_contact(
+    app.session.login(username="admin", password="secret")
+    app.contact.create(
         Contact(firstname="Olya", middlename="blabla", lastname="Popova", nickname="blablaolya", title="test",
                 company="OOO \"test\"", address="ul testovskaya", home="1",
                 mobile="+7909090", work="tester", fax="+79898998", email="blabla@mail.ru", email2="blabla2@mail.ru",
                 homepage="https://www.youtube.com/", email3="blabla3@mail.ru", bday="1", bmonth="January", byear="1992",
                 aday="2", amonth="February",
                 ayear="1993", new_group="test", address2="ul energeticheskaya", phone2="2", notes="test test"))
-    app.logout()
+    app.session.logout()
