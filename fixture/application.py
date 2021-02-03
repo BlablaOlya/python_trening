acef0885215ce634fbdc7644cfb8005097985acc
Popova Olga Olegovna <olgapo@ozon.ru>
2021-02-03 19:59:51 +0300

    add modify test. выделение вспомогательных методов fill_group_form, change_field_value, select_first_group, modify_first_group. Изменение group.py: параметры None

--- fixture/application.py
+++ fixture/application.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
@@ -7,6 +8,7 @@ from fixture.contact import ContactHelper
 class Application:
 
     def __init__(self):
+        # self.wd = webdriver.Chrome(ChromeDriverManager().install())
         self.wd = webdriver.Chrome()
         self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
